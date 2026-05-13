6d727855dd12119c22d8fe7c9c446529ac92ef73
Joel Castaneda <iscjoelcastaneda@outlook.com>
2026-05-13 14:03:38 -0600

    Prmer push con estructura base y conf. gitignore

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// ============================================================
+// SISTEMA DE INVENTARIO - Clase 1.1
+// Estado: Mensaje de bienvenida
+// ============================================================
+
+Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión: 1.0.0");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+Console.WriteLine("Estructura del proyecto");
+Console.WriteLine("Configuracion .csproject");
+Console.WriteLine("Carpeta src/ creada");
+Console.WriteLine("Metadatos Configurados");
+Console.WriteLine();
+Console.WriteLine("Proximo Paso: Agregar argumentos CLI y configuracion de repositorio en Github");
